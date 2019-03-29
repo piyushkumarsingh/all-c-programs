b7889d4bcaf4e7593acc27402a17279e66120b30
piyushkumarsingh <41803008+piyushkumarsingh@users.noreply.github.com>
2019-03-29 14:38:39 +0530

    even odd program

---
+++ exam.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+ main()
+ {
+ 	int i;
+ 	printf("enter the number");
+ 	scanf("%d ",&i);
+ 	 if(i%2==0)
+ 	 printf("the number is even");
+ 	 else
+ 	 printf("the number is odd");
+ 	
+ }
